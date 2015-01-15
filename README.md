cb03a4eb973ce2bed48c812088f5010ffa534d92
snillocmot <tomcollins@globo-smart.com>
2015-01-15 17:45:22 +0100

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Node.js SmartLiving/AllThingsTalk library
+
+Use this to connect your apps, services, controllers... cat, dog, grandmother to the SmartLiving IoT Cloud Platform.
+
+	1. git clone http://github.com/allthingstalk/nodejs-client
+	2. npm install
+	3. node cli-sensor
+
+Donezo.
---
+++ macos_cpu_temp.js
@@ -0,0 +1,17 @@
+var smartliving = require('./lib/standard');
+var smc = require('smc');
+
+smartliving.DeviceId = ("YOUR-DEVICEID");
+smartliving.ClientId = ("YOUR-CLIENTID");
+smartliving.ClientKey = ("YOUR-CLIENTKEY");
+
+smartliving.addAsset("1", "Mac CPU Temp", "Monitors the temperature of the top secret nuclear reactor controller... AKA my Mac ", false, "int", function(){console.log("Mac CPU temperature sensor enrolled")});
+
+smartliving.subscribe(smartliving.baseMQTTUrl, 1883);
+vloop(); 
+
+function vloop()
+{
+  smartliving.send(smc.temperature(), "1"); //This "1" is nasty... Let's pass it the asset object instead   
+  setTimeout(vloop,1000); 
+}
